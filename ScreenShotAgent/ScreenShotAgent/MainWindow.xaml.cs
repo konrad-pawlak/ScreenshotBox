0614e13079c967086dc8ac706ed47dcea0ad96ea
Radkoncy <radkoncy@gmail.com>
2017-12-28 16:10:39 +0000

    Added icon for Notification area

--- ScreenShotAgent/ScreenShotAgent/MainWindow.xaml.cs
+++ ScreenShotAgent/ScreenShotAgent/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Forms;
 
 namespace ScreenShotAgent
 {
@@ -20,9 +8,28 @@ namespace ScreenShotAgent
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		NotifyIcon notifyIcon = new NotifyIcon();
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			Closing += MainWindow_Closing;
+
+			MinimizeAppToNotifyArea();
+		}
+
+		private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			notifyIcon.Visible = false;
+		}
+
+		private void MinimizeAppToNotifyArea()
+		{
+			WindowState = WindowState.Minimized;
+
+			notifyIcon.Icon = Properties.Resources.status_bar_icon;
+			notifyIcon.ShowBalloonTip(5000, "Hi", "This is a BallonTip from Windows Notification", ToolTipIcon.Info);
+			notifyIcon.Visible = true;
 		}
 	}
 }
