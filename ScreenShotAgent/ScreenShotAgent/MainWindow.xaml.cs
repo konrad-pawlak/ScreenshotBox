a9dd4c8a6f1790928dc7800ab4289d8fa41c12a3
Radkoncy <radkoncy@gmail.com>
2017-12-28 16:50:43 +0000

    Added context menu for notification area and window closing event

--- ScreenShotAgent/ScreenShotAgent/MainWindow.xaml.cs
+++ ScreenShotAgent/ScreenShotAgent/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Forms;
 
 namespace ScreenShotAgent
@@ -13,23 +14,50 @@ namespace ScreenShotAgent
 		public MainWindow()
 		{
 			InitializeComponent();
-			Closing += MainWindow_Closing;
+			Hide();
 
-			MinimizeAppToNotifyArea();
+			PrepareNotificationIcon();
+			RegisterWindowEvents();
+		}
+
+		private void PrepareNotificationIcon()
+		{
+			CreateNotificationIcon();
+			SetContextMenuForNotificationIcon();
+		}
+
+		private void CreateNotificationIcon()
+		{
+			notifyIcon.Icon = Properties.Resources.status_bar_icon;
+			notifyIcon.Visible = true;
+			notifyIcon.Text = "ScreenShot Agent";
+		}
+
+		private void SetContextMenuForNotificationIcon()
+		{
+			notifyIcon.ContextMenu = new ContextMenu();
+			notifyIcon.ContextMenu.MenuItems.Add(new MenuItem("Settings", OpenSettigns));
+			notifyIcon.ContextMenu.MenuItems.Add(new MenuItem("Exit", CloseApplication));
+		}
+
+		private void OpenSettigns(object sender, EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void CloseApplication(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void RegisterWindowEvents()
+		{
+			Closing += MainWindow_Closing;
 		}
 
 		private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
 		{
 			notifyIcon.Visible = false;
-		}
-
-		private void MinimizeAppToNotifyArea()
-		{
-			WindowState = WindowState.Minimized;
-
-			notifyIcon.Icon = Properties.Resources.status_bar_icon;
-			notifyIcon.ShowBalloonTip(5000, "Hi", "This is a BallonTip from Windows Notification", ToolTipIcon.Info);
-			notifyIcon.Visible = true;
 		}
 	}
 }
