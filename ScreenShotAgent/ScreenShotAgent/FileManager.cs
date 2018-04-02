b6fd5841faecb096ba4808edfd377efd4ef69728
Radkoncy <radkoncy@gmail.com>
2018-04-02 22:42:28 +0100

    Filemanager - changed filename format to be able to save multiple screenshots at the same second. Also a small hotfix for null exception.

--- ScreenShotAgent/ScreenShotAgent/FileManager.cs
+++ ScreenShotAgent/ScreenShotAgent/FileManager.cs
@@ -8,10 +8,13 @@ namespace ScreenshotBox
 	{
 		public static void SaveBitmap(BitmapSource image)
 		{
+			if (image == null)
+				return; //Add logging if file logging.on exists
+
 			var encoder = PngEncoder;
 			encoder.Frames.Add(BitmapFrame.Create(image));
 
-			var stream = new FileStream(DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss") + "new.png", FileMode.Create);
+			var stream = new FileStream("screenshoot--" + DateTime.Now.ToString("yyyy-MM-dd--HH-mm-ss.fff") + ".png", FileMode.Create);
 			//TODO: Add counter to the screen name if the screenshots were taken at the same second
 
 			encoder.Save(stream);
